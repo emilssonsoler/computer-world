5f8aa8d49ae140c22a37b864a4adeef93f8d4aa7
Emilsson Soler <edennirs@gmail.com>
2022-06-03 17:50:19 -0600

    Agregamos una lista de productos

---
+++ ComputerWorld/ComputerWorld.Web/Controllers/ProductosController.cs
@@ -0,0 +1,32 @@
+﻿using ComputerWorld.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComputerWorld.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto = new ProductoModel();
+            producto.Id = 1;
+            producto.Descripcion = "Computadora 1";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Computadora 2";
+
+            var listaDeProductos = new List<ProductoModel>();
+            listaDeProductos.Add(producto);
+            listaDeProductos.Add(producto2);
+
+
+
+            return View(listaDeProductos);
+        }
+    }
+}
